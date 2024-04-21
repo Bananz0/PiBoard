99b046da65b993691421ed03b4bdd08caa776130
glenm@XPS <glenmuthokaq@gmail.com>
2024-04-21 11:52:53 +0100

    added dummy wiringPi functinos

--- minerva.cpp
+++ minerva.cpp
@@ -1,7 +1,17 @@
 //Comms
 #include "minerva.h"
 
-Minerva::Minerva() {}
+//enable when wiringPi is installed
+//#include "wiringPi.h"
+#define OUTPUT 1
+#define INPUT 0
+#define HIGH 1
+#define LOW 0
+
+
+Minerva::Minerva() {
+
+}
 
 void Minerva::getServerWindowSize(QSize hermesSize){
     winSize = hermesSize;
@@ -12,10 +22,39 @@ QSize Minerva::setClentWindowSize(){
 }
 
 void Minerva::testConnection(){
-
+    if (/*Implement Test Connection*/ 1) {
+		qDebug() << "Connection failed";
+    }
+    else {
+		qDebug() << "Connection successful";
+	}
 };
 
-void Minerva::selectDataPin(int pinNumber){
-
+void Minerva::selectDataPin(int pinNumber,int dataModeNum){
+    pinMode(dataPins[pinNumber], dataModeNum);
 };
 
+void Minerva::initializeGPIO() {
+    wiringPiSetupGpio();
+}
+
+void Minerva::serverMode() {
+    pinMode(18, OUTPUT);
+	digitalWrite(18, HIGH);
+}
+
+
+void Minerva::clientMode() {
+    Minerva::selectDataPin(0, 0);
+}
+
+//dummy function to comly with wiringPi
+void Minerva::digitalWrite(int pin, int value) {
+	//dummy function
+}
+void Minerva::pinMode(int pin, int mode) {
+	//dummy function
+}
+void Minerva::wiringPiSetupGpio() {
+
+}
--- minerva.h
+++ minerva.h
@@ -1,19 +1,29 @@
 #ifndef MINERVA_H
 #define MINERVA_H
 
-//#include wiringPi.h
 #include <QSize>
 
 class Minerva
 {
+private:
+    int dataPins[10] = {25,28,24,27,22,26,30,31,14,10};
 public:
     QSize winSize;
     Minerva();
     void testConnection();
+    void selectDataPin(int pinNumber, int dataModeNum);
     void getServerWindowSize(QSize hermesSize);
     QSize setClentWindowSize();
-    void selectDataPin(int pinNumber);
-    Minerva *minerva = new Minerva;
+    void initializeGPIO();
+    void serverMode();
+    void clientMode();
+
+    
+
+    //dummy function to comly with wiringPi
+    void digitalWrite(int pin, int value);
+    void pinMode(int pin, int mode);
+    void wiringPiSetupGpio();
 };
 
 #endif // MINERVA_H
