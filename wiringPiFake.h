683709ef0150a998242f95005702825b9a06f5ca
glenm@XPS <glenmuthokaq@gmail.com>
2024-04-30 07:36:45 +0100

    better modified the wiringPiFake.h to properly include wiringPi when in luunix and not in windows

--- wiringPiFake.h
+++ wiringPiFake.h
@@ -1,8 +1,14 @@
-//enable when wiringPi is installed
-#define WIRINGPI
-#ifdef WIRINGPI
+// Fake library for wiringPi to allow compilation on Windows cause the Pi is so slow
 
-////dummy defines for wiringPi
+#define WIRINGPIFAKE_H
+#ifdef WIRINGPIFAKE_H
+
+
+#ifdef __linux__
+// Include the actual wiringPi library for Linux
+#include <wiringPi.h>
+#else
+// Declare dummy functions for Windows
 #define OUTPUT 1
 #define INPUT 0
 #define HIGH 1
@@ -22,8 +28,29 @@ int digitalRead(int pinNumber) {
     return 0;
 }
 void delay(int delayTime) {
+    //dummy function
+}
+void delayMicroseconds(int delayTime) {
 	//dummy function
 }
-
-
+int wiringPiSetup() {
+	//dummy function
+	return 0;
+}
+void pullUpDnControl(int pin, int pud) {
+	//dummy function
+}
+void wiringPiISR(int pin, int mode, void (*function)(void)) {
+	//dummy function
+}
+void pwmWrite(int pin, int value) {
+	//dummy function
+}
+void softPwmCreate(int pin, int value, int range) {
+	//dummy function
+}
+void softPwmWrite(int pin, int value) {
+	//dummy function
+}
+#endif
 #endif // WIRINGPIFAKE_H
